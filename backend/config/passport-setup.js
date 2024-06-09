55d36d3c34aac6ca7ab8367d29a1ab98b287e958
JANGYUN <1801406@donga.ac.kr>
2024-06-10 02:30:12 +0900

    이슈 해결

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -8,7 +8,7 @@ dotenv.config({ path: './src/routes/.env' });
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "http://localhost:3000/auth/google/callback/"
 },
 async function(token, tokenSecret, profile, done) {
   try {
--- backend/src/routes/server.js
+++ backend/src/routes/server.js
@@ -1,23 +1,10 @@
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const bodyParser = require('body-parser');
-const passport = require('passport');
 const session = require('express-session');
+const passport = require('passport');
+require('./../../config/passport-setup'); // 경로를 맞게 설정해주세요
 const authRouter = require('./authRouter');
-const profileRouter = require('./profileRouter');
-const virtualCompilerRouter = require('./virtualCompilerRouter');
-const signUpRouter = require("./signUpRouter");
-const loginRouter = require("./loginRouter");
-const mypageRouter = require("./mypageRouter");
-
-require('../../config/passport-setup'); // 경로 수정
-dotenv.config({ path: './src/routes/.env' });
 
 const app = express();
-
-app.use(cors());
-app.use(express.json());
 
 app.use(session({
   secret: 'your-secret-key',
@@ -28,14 +15,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/sign", signUpRouter);
-app.use("/login", loginRouter);
-app.use('/api', virtualCompilerRouter);
-app.use("/mypage", mypageRouter);
 app.use('/auth', authRouter);
-app.use('/', profileRouter);
 
-console.log('MAIL_REFRESH:', process.env.MAIL_REFRESH);
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
