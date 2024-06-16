37d8b0b2401500ad371923ca8538b7aaf62d034b
JANGYUN <1801406@donga.ac.kr>
2024-06-16 23:56:28 +0900

    nickname..

--- backend/src/controllers/feedbackController.js
+++ backend/src/controllers/feedbackController.js
@@ -7,23 +7,19 @@ exports.createFeedback = async (req, res) => {
   const { post_id, user_id, feedback_comment, feedback_codenumber } = req.body;
   
   try {
-    // 현재 날짜와 시간을 가져오기
-    const feedbackDate = new Date();
-    const formattedDate = feedbackDate.toISOString().slice(0, 19).replace('T', ' ');
-
     // user_id로 user_nickname 조회
     const [userRows] = await pool.query('SELECT user_nickname FROM user WHERE user_id = ?', [user_id]);
     if (userRows.length === 0) {
       return res.status(404).json({ error: 'User not found' });
     }
     const user_nickname = userRows[0].user_nickname;
-    console.log('Feedback date:', formattedDate);
+
+    // 현재 날짜와 시간을 가져오기
+    const feedbackDate = new Date();
+    const formattedDate = feedbackDate.toISOString().slice(0, 19).replace('T', ' ');
 
     // 피드백 생성
     const feedbackQuery = 'INSERT INTO feedback (post_id, user_id, user_nickname, feedback_date, feedback_comment, feedback_codenumber) VALUES (?, ?, ?, ?, ?, ?)';
-    console.log('Executing query:', feedbackQuery);
-    console.log('With values:', [post_id, user_id, user_nickname, formattedDate, feedback_comment, feedback_codenumber]);
-
     const [feedbackResult] = await pool.query(feedbackQuery, [post_id, user_id, user_nickname, formattedDate, feedback_comment, feedback_codenumber]);
 
     res.status(201).json(feedbackResult);
@@ -49,7 +45,6 @@ exports.getFeedbacksByPostId = async (req, res) => {
   const post_id = req.params.post_id;
   try {
     const query = 'SELECT * FROM feedback WHERE post_id = ?';
-    console.log(`Executing query: ${query} with post_id: ${post_id}`); // 디버깅 로그 추가
     const [rows] = await pool.query(query, [post_id]);
     res.json(rows);
   } catch (error) {
