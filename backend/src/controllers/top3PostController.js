3eabff88a11d5b98bd7d87501ece745dd8d32160
LSjin16 <leesj5692@gmail.com>
2024-06-20 23:33:49 +0900

    기여도

--- backend/src/controllers/top3PostController.js
+++ backend/src/controllers/top3PostController.js
@@ -2,19 +2,20 @@
 const pool = require('../../config/databaseSet');
 
 // 최근 2주간의 게시물 중에서 좋아요가 많은 상위 3개를 가져오는 함수
-exports.getTop3Posts = async (req, res) => {
+exports.getTop3Posts = async (req, res) => {  
   const twoWeeksAgo = new Date();
   twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
   const twoWeeksAgoFormatted = twoWeeksAgo.toISOString().split('T')[0];
 
   const sqlQuery = `
-    SELECT p.*, COUNT(pl.post_id) AS total_likes 
+    SELECT p.*, u.user_contribute, COUNT(pl.post_id) AS total_likes 
     FROM post p
     LEFT JOIN post_likes pl ON p.post_id = pl.post_id
+    LEFT JOIN user u ON p.user_id = u.user_id
     WHERE p.post_date >= ?
-    GROUP BY p.post_id 
+    GROUP BY p.post_id, u.user_contribute
     ORDER BY total_likes DESC
-    LIMIT 3
+    LIMIT 3;
   `;
 
   try {
@@ -27,30 +28,4 @@ exports.getTop3Posts = async (req, res) => {
   }
 };
 
-// src/controllers/feedbackController.js
 
-// 최근 2주간의 댓글 중에서 좋아요가 많은 상위 3개를 가져오는 함수
-exports.getTop3Feedback = async (req, res) => {
-  const twoWeeksAgo = new Date();
-  twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
-  const twoWeeksAgoFormatted = twoWeeksAgo.toISOString().split('T')[0];
-
-  const sqlQuery = `
-    SELECT f.*, COUNT(fl.feedback_id) AS total_likes 
-    FROM feedback f
-    LEFT JOIN feedback_likes fl ON f.feedback_id = fl.feedback_id
-    WHERE f.feedback_date >= ?
-    GROUP BY f.feedback_id 
-    ORDER BY total_likes DESC
-    LIMIT 3
-  `;
-
-  try {
-    console.log('Executing query:', sqlQuery);
-    const [results] = await pool.query(sqlQuery, [twoWeeksAgoFormatted]);
-    res.json(results);
-  } catch (err) {
-    console.error('Database error:', err);
-    res.status(500).json({ error: 'Database error' });
-  }
-};
